b0116d3b3627403865897fca7a2c3bebe9158ca8
andrew.chaa <andrew.chaa@huddle.com>
2014-07-18 18:06:31 +0100

    Send the text to DisplayActivity

--- app/src/main/java/com/example/andrewchaa/learningandroid/DisplayMessageActivity.java
+++ app/src/main/java/com/example/andrewchaa/learningandroid/DisplayMessageActivity.java
@@ -1,24 +1,35 @@
 package com.example.andrewchaa.learningandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.example.andrewchaa.learningandroid.R;
 
-public class MyActivity extends Activity {
+public class DisplayMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.display_message, menu);
         return true;
     }
 
